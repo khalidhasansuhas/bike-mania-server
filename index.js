30d9046b68373dac07cb1fd1692df865f553789f
khalidhasansuhas <khalidhasansuhas@gmail.com>
2022-11-30 03:50:33 +0600

    jwt token added

--- index.js
+++ index.js
@@ -14,12 +14,33 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d5moryc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: " UnAuthorized Access" })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: " Forbidden Access" })
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
+
 async function run() {
     try {
         const usersCollection = client.db('bikeMania').collection('users')
         const bikesCollection = client.db('bikeMania').collection('bikes')
         const categoriesCollection = client.db('bikeMania').collection('categories')
         const bookingsCollection = client.db('bikeMania').collection('bookings')
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10h' });
+            res.send({ token })
+        })
 
         app.get('/categories', async(req, res)=>{
             const query = {}
