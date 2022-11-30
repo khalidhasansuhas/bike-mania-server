3cec849e85eef6e56b5841baf4b841fb9d03a15d
khalidhasansuhas <khalidhasansuhas@gmail.com>
2022-11-30 10:51:44 +0600

    verify admin and seler created

--- index.js
+++ index.js
@@ -63,11 +63,18 @@ async function run() {
             res.send(result);
         });
 
-        app.get('/users/admin/:id', async(req, res)=>{
-            const id = req.params.id;
-            const query = {_id: ObjectId(id)}
+        app.get('/users/admin/:email', async(req, res)=>{
+            const email = req.params.email;
+            const query = {email}
             const user = await usersCollection.findOne(query);
             res.send({isAdmin : user?.role === 'admin'});
+        })
+
+        app.get('/users/seller/:email', async(req, res)=>{
+            const email = req.params.email;
+            const query = {email}
+            const user = await usersCollection.findOne(query);
+            res.send({isSeller : user?.role === 'seller'});
         })
        
 
